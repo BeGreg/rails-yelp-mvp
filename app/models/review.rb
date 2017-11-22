b2dec23c0d2b2a5bc1199d33be629b7a36579e80
gregory blain <gregory.blain@gmail.com>
2017-11-22 11:50:47 +0100

    AJout validation et migrate

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
+  validates :restaurant_id, presence: true
+  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4 ,5] }, numericality: true
+  validates :content, presence: true
   belongs_to :restaurant
 end
